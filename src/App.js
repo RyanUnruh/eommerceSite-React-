c5e341dd6582869cd8dc951522bc79ddea13990d
Ryan Unruh <ryanunruhmedia@gmail.com>
2020-05-01 18:09:54 -0500

    Sign in page created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./components/pages/homepage/homepage.component";
 import ShopPage from "./components/pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInSignOut from "./components/pages/sign-in-and-sign-up/sign-in and-sign-up.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignOut} />
       </Switch>
     </div>
   );
---
+++ src/components/pages/sign-in-and-sign-up/sign-in and-sign-up.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./sign-in-sign-up.styles.scss";
+const SignInSignOut = () => {
+  return <div className="Sign-in-and-sign-out">SIGN IN</div>;
+};
+
+export default SignInSignOut;
