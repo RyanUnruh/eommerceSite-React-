7022ac4e47a8bccd91c035fee137cc7015fe7ca5
Ryan Unruh <ryanunruhmedia@gmail.com>
2020-05-03 13:08:19 -0500

    Sign in component created with styling

--- src/components/pages/sign-in-and-sign-up/sign-in and-sign-up.component.jsx
+++ src/components/pages/sign-in-and-sign-up/sign-in and-sign-up.component.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./sign-in-sign-up.styles.scss";
+import SignIn from "../../sign-in/sign-in.component";
+
 const SignInSignOut = () => {
-  return <div className="Sign-in-and-sign-out">SIGN IN</div>;
+  return (
+    <div className="Sign-in-and-sign-out">
+      <SignIn />
+    </div>
+  );
 };
 
 export default SignInSignOut;
