9f9e67a32653925f9b993e5e71e4f8c38957ac92
Ryan Unruh <ryanunruhmedia@gmail.com>
2020-05-26 22:28:47 -0500

    Sign up edit

--- src/components/pages/sign-in-and-sign-up/sign-in and-sign-up.component.jsx
+++ src/components/pages/sign-in-and-sign-up/sign-in and-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import "./sign-in-sign-up.styles.scss";
+import "./sign-in-and-sign-up.scss";
 import SignIn from "../../sign-in/sign-in.component";
+import SignUp from "../../sign-up/sign-up.component";
 
 const SignInSignOut = () => {
   return (
     <div className="Sign-in-and-sign-out">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
